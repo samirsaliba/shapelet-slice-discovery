92565bc950b51151969a2e87942b6bb0ef3113ff
Samir Saliba <samirtsj@gmail.com>
2024-06-18 20:58:33 -0300

    Add subgroup size factor and coverage weight;

--- gendis/subgroup_distance.py
+++ gendis/subgroup_distance.py
@@ -16,17 +16,17 @@ class SubgroupDistance:
         self,
         distance_function, 
         shapelet_dist_threshold, 
-        min_support,
         cov_alpha,
         cov_weights,
         coverage_weights=None,
+        sg_size_beta=2,
         standardize=False
     ):
         self.cov_alpha = cov_alpha
         self.cov_weights = cov_weights
+        self.sg_size_beta = sg_size_beta
         self.shapelet_dist_threshold = shapelet_dist_threshold
         self.distance_function = distance_function
-        self.min_support = min_support if min_support is not None else -1
         self.standardize = standardize
 
     def __call__(self, D, y, shaps=None):
@@ -34,6 +34,10 @@ class SubgroupDistance:
 
     def update_weights(self, cov_weights):
         self.cov_weights=cov_weights
+
+    def subgroup_size_factor(self, subgroup_n, y_n):
+        """Score that favors larger subgroups"""
+        return (subgroup_n / y_n) ** self.sg_size_beta
 
     def coverage_factor(self, subgroup):
         """Multiplicative weighted covering score"""
@@ -71,22 +75,25 @@ class SubgroupDistance:
         subgroup_y = y[subgroup]
         subgroup_n = sum(subgroup)
 
-        fitness = -np.inf
-        subgroup_mean_distance = np.inf
-        covW = None
-        dist = None
+        if subgroup_n < 10:
+            fitness = -np.inf
+            covW = None
+            sizeW = None
+            dist = None
 
-        if subgroup_n  >= self.min_support:
-            subgroup_mean_distance = D[subgroup].mean(0).min()
+        else:
             dist = self.distance_function(subgroup_y, y)
             covW = self.coverage_factor(subgroup)
-            fitness =  covW * dist
+            sizeW = self.subgroup_size_factor(subgroup_n, len(y))
+            fitness =  dist * covW * sizeW
 
         sg_mean = np.mean(subgroup_y)
         return {
             'value': np.array([fitness]),
             'info': {
+                'fitness': fitness,
                 'dist': dist,
+                'subgroup_size_weight': sizeW,
                 'coverage_weight': covW,
                 'subgroup_error_mean': sg_mean,
                 'population_mean': np.mean(y),
